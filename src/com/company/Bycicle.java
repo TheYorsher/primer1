c2bfb829060bbfa666b57b5f7fde7fe96ae5e227
TheYorsher <stepanov2003@mail.ru>
2019-12-04 21:45:37 +0300

    Initial commit

---
+++ src/com/company/Bycicle.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Bycicle extends Transport {
+    @Override
+    void beep() {
+        System.out.println("no beep");
+    }
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Transport car = new Car();
+	car.beep();
+	Transport bycicle = new Bycicle();
+	bycicle.beep();
+    }
+}
